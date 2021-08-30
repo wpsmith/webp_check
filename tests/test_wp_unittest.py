2c43a1c447cde66529022f965c7a49cc620cc2c5
Travis Smith <t@wpsmith.net>
2021-08-30 11:30:54 -0400

    Fix search_replace tests

--- tests/test_wp_unittest.py
+++ tests/test_wp_unittest.py
@@ -1,4 +1,5 @@
 import unittest
+from subprocess import CalledProcessError
 
 import wp
 import config
@@ -31,14 +32,50 @@ class TestWPConfig(unittest.TestCase):
     def test_search_replace(self):
         sr = wp.SearchReplace("old-dne-str", "new-dne-str",
                               table="wp_post*",
+                              dry_run=True,
                               all_tables_with_prefix=True,
                               path="/Users/travis.smith/Projects/WPS/paideiasoutheast.test",
                               url="domain.com")
-        # self.assertEqual(['search-replace'], sr._cmd(), "cmd: Should be ['search-replace']")
-        # self.assertEqual(["'old-dne-str'", "'new-dne-str'", 'wp_post*', '--all-tables-with-prefix', '--url=domain.com'], sr._params(), "_params: Should be [\"'old-dne-str'\", \"'new-dne-str'\", 'wp_post*', '--all-tables-with-prefix', '--url=domain.com']")
-        # self.assertEqual(['wp', 'search-replace', "'old-dne-str'", "'new-dne-str'", 'wp_post*', '--all-tables-with-prefix', '--url=domain.com'], sr.cmd(), "cmd: Should be ['wp', 'search-replace', 'old', 'new', 'wp_post*]")
-        self.assertEqual([], sr.run(), "run: ")
+        self.assertEqual(['search-replace'],
+            sr._cmd(),
+            "cmd: Should be ['search-replace']")
 
+        expected_output = [
+            "\"old-dne-str\"",
+            "\"new-dne-str\"",
+            "\"wp_post*\"",
+            '--all-tables-with-prefix',
+            '--dry-run',
+            '--path="/Users/travis.smith/Projects/WPS/paideiasoutheast.test"',
+            '--url="domain.com"'
+        ]
+        self.assertEqual(
+            expected_output,
+            sr._params(),
+            "_params: Should be [{0}]\n".format(",\n".join(expected_output)))
+
+        expected_output = [
+            'wp',
+            'search-replace',
+            "\"old-dne-str\"",
+            "\"new-dne-str\"",
+            "\"wp_post*\"",
+            '--all-tables-with-prefix',
+            '--dry-run',
+            '--path="/Users/travis.smith/Projects/WPS/paideiasoutheast.test"',
+            '--url="domain.com"'
+        ]
+        self.assertEqual(
+            expected_output,
+            sr.cmd(),
+            self.assert_msg('cmd', expected_output))
+
+        # Test invalid path.
+        with self.assertRaises(CalledProcessError):
+            sr.run()
+
+    def assert_msg(self, cmd_name, value):
+        return "{0}: Should be [{1}]\n".format(cmd_name, ",\n".join(value))
 
 
 if __name__ == '__main__':
