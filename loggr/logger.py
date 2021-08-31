c1b375b7465bc832e2ccc2ebed962dc92d93b568
Travis Smith <t@wpsmith.net>
2021-08-31 16:36:03 -0400

    Make sure logpath exists

--- loggr/logger.py
+++ loggr/logger.py
@@ -7,6 +7,10 @@ logpath = config.get().get('LOG_PATH')
 if logpath is None or "" == logpath:
     logpath = "."
 
+# Make sure logpath exists
+if not os.path.exists(logpath):
+    os.makedirs(logpath)
+
 logfile = os.path.join(logpath, "logs.log")
 logging.basicConfig(filename=logfile, filemode='w', format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger()
