a8f314b620caa5391e1418494770c8d341bbf126
Travis Smith <t@wpsmith.net>
2021-08-31 16:34:49 -0400

    Update logger with config

--- loggr/logger.py
+++ loggr/logger.py
@@ -3,7 +3,11 @@ import os
 
 import config
 
-logfile = os.path.join(config.get().get('LOG_PATH'), "logs.log")
+logpath = config.get().get('LOG_PATH')
+if logpath is None or "" == logpath:
+    logpath = "."
+
+logfile = os.path.join(logpath, "logs.log")
 logging.basicConfig(filename=logfile, filemode='w', format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
