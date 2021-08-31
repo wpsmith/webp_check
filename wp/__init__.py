71ac60ad0a72c33fcdaeceb7f86018c27eca3e69
Travis Smith <t@wpsmith.net>
2021-08-31 15:40:28 -0400

    Add some commands

--- wp/__init__.py
+++ wp/__init__.py
@@ -1,4 +1,28 @@
 from wp.config import WPCLIConfig
-from wp.wp import WP, wp
+# from wp.wp import WP, wp
+
+# search-replace
 from wp.search_replace import SearchReplace
-from wp.command import WPCommand
+
+# db
+from wp.db_prefix import DBPrefix
+from wp.db_query import DBQuery
+from wp.db_check import DBCheck
+from wp.db_import import DBImport
+from wp.db_export import DBExport
+from wp.db_search import DBSearch
+from wp.db_columns import DBColumns
+from wp.db_tables import DBTables
+from wp.db_items import DBItems, DBItem
+
+# post
+from wp.post_get import PostGet
+from wp.post_update import PostUpdate
+
+# option
+from wp.option_add import OptionAdd
+from wp.option_get import OptionGet
+from wp.option_update import OptionUpdate
+
+# cache
+from wp.cache_flush import CacheFlush
