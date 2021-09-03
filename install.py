ab462c390cf4f50aec4636fa0b768ca318f44282
Travis Smith <t@wpsmith.net>
2021-09-02 21:55:55 -0400

    Install cronjob

--- install.py
+++ install.py
@@ -1,28 +1,15 @@
 import os
-import subprocess
-from urllib import request
 
 from crontab import CronTab
 
-
+# Installs the cron job.
 def install_cron():
     cron = CronTab()
-    cron.new(command='my command', comment='my comment')
-
-    job = cron.new(command='python ' + os.path.join(os.path.dirname(__file__), "webp_check.py"))
-    job.minute.every(1)
-    job.hour.every(1)
+    job = cron.new(command=os.path.join(os.path.dirname(__file__), "check"))
+    job.minute.every(15)
+    # job.hour.every(1)
 
     cron.write()
 
 
-def download_wp_cli():
-    wp_cli_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "wp")
-    request.urlretrieve("https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar", wp_cli_path)
-    subprocess.call(["chmod", "+x", wp_cli_path])
-    subprocess.call(["sudo", "mv", wp_cli_path, "/usr/local/bin/wp2"])
-    output = subprocess.check_output(["wp", "--info"])
-    print(output)
-
-
-download_wp_cli()
+install_cron()
