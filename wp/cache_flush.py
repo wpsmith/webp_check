9675f7a46542a25dd5ec27d2c75039dc0c85b19a
Travis Smith <t@wpsmith.net>
2021-09-02 21:55:04 -0400

    Add docs

--- wp/cache_flush.py
+++ wp/cache_flush.py
@@ -1,6 +1,13 @@
 from wp.command import WPCommand
 
 
+# Flushes the object cache.
+# For WordPress multisite instances using a persistent object cache,
+# flushing the object cache will typically flush the cache for all sites.
+# Beware of the performance impact when flushing the object cache in production.
+# Errors if the object cache can’t be flushed.
+# wp cache flush
+# Success: The cache was flushed.
 class CacheFlush(WPCommand):
     command = ['cache', 'flush']
 
@@ -9,4 +16,3 @@ class CacheFlush(WPCommand):
 
     def params(self):
         return []
-
