1c97d293b8dd3789c87bcb37ea645dca34113eca
Travis Smith <t@wpsmith.net>
2021-10-05 20:10:21 -0400

    Explicitly set crontab file and user

--- install.py
+++ install.py
@@ -4,11 +4,9 @@ from crontab import CronTab
 
 # Installs the cron job.
 def install_cron():
-    cron = CronTab()
+    cron = CronTab(tabfile='/etc/crontab', user=False)
     job = cron.new(command=os.path.join(os.path.dirname(__file__), "check"))
     job.minute.every(15)
-    # job.hour.every(1)
-
     cron.write()
 
 
